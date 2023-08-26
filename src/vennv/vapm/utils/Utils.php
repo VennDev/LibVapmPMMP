400eac8058df3fcacc6733c7653e776764a1a5c5
VennV <111500380+VennDev@users.noreply.github.com>
2023-08-26 19:00:22 +0700

    Update Utils.php

--- src/vennv/vapm/utils/Utils.php
+++ src/vennv/vapm/utils/Utils.php
@@ -23,6 +23,7 @@ declare(strict_types = 1);
 
 namespace vennv\vapm\utils;
 
+use vennv\vapm\Error;
 use Closure;
 use Generator;
 use RecursiveDirectoryIterator;
@@ -30,7 +31,6 @@ use RecursiveIteratorIterator;
 use ReflectionException;
 use ReflectionFunction;
 use SplFileInfo;
-use vennv\vapm\simultaneous\Error;
 use function array_slice;
 use function file;
 use function implode;
